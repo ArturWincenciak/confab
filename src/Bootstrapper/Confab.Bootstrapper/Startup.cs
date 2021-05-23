71dbe8edc0687c39232ae43d8ae0f243f9fe678c
TeoVincent <artur.wincenciak@gmail.com>
2021-05-23 23:01:15 +0200

    Init (dir structure, empty Bootstrap ASP .NET Core App).

---
+++ src/Bootstrapper/Confab.Bootstrapper/Startup.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Confab.Bootstrapper
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context => await context.Response.WriteAsync("Confab API!"));
+            });
+        }
+    }
+}
