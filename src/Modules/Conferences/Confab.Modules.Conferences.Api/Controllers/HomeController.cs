90a0e500355b73f6617b678f9f7f839da064975e
TeoVincent <artur.wincenciak@gmail.com>
2021-05-24 23:14:08 +0200

    Create hello world controller method in Conferences module.

---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Api/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Confab.Modules.Conferences.Api.Controllers
+{
+    [Route("conferences-module")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Conferences API";
+    }
+}
