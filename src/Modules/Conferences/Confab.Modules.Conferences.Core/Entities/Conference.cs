28572135493a87fa0d911971749d5621064efc1f
TeoVincent <artur.wincenciak@gmail.com>
2021-05-28 21:37:41 +0200

    Create Conference and Host anemic model (this is just CRUD).

---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Entities/Conference.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Confab.Modules.Conferences.Core.Entities
+{
+    internal class Conference
+    {
+        public Guid Id { get; set; }
+        public Guid HostId { get; set; }
+        public Host Host { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Localization { get; set; }
+        public string LogoUrl { get; set; }
+        public int? ParticipantsLimit { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
