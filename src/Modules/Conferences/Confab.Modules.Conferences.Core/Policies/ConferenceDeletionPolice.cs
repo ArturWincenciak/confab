4153285d13f3b98900b94bd7a0532c376cc9ad33
TeoVincent <artur.wincenciak@gmail.com>
2021-06-02 22:12:35 +0200

    Impl Host and Conferences deletion polices.

---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Policies/ConferenceDeletionPolice.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Confab.Modules.Conferences.Core.Entities;
+using Confab.Shared.Abstractions;
+
+namespace Confab.Modules.Conferences.Core.Policies
+{
+    internal class ConferenceDeletionPolice : IConferenceDeletionPolice
+    {
+        private readonly IClock _clock;
+
+        public ConferenceDeletionPolice(IClock clock)
+        {
+            _clock = clock;
+        }
+
+        public Task<bool> CanDeleteAsync(Conference conference)
+        {
+            //TODO: Check if there any participants?
+            var canDelete = _clock.CurrentDate().Date.AddDays(7) < conference.From.Date;
+            return Task.FromResult(canDelete);
+        }
+    }
+}
---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Policies/HostDeletionPolice.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Confab.Modules.Conferences.Core.Entities;
+
+namespace Confab.Modules.Conferences.Core.Policies
+{
+    internal class HostDeletionPolice : IHostDeletionPolice
+    {
+        private readonly IConferenceDeletionPolice _conferenceDeletionPolice;
+
+        public HostDeletionPolice(IConferenceDeletionPolice conferenceDeletionPolice)
+        {
+            _conferenceDeletionPolice = conferenceDeletionPolice;
+        }
+
+        public async Task<bool> CanDeleteAsync(Host host)
+        {
+            if (host.Conferences is null || !host.Conferences.Any())
+            {
+                return true;
+            }
+
+            foreach (var conference in host.Conferences)
+            {
+                if (await _conferenceDeletionPolice.CanDeleteAsync(conference) is false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
