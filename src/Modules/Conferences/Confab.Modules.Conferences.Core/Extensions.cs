68d44e3b0b4635209daf54893070f3bfa7b54861
TeoVincent <artur.wincenciak@gmail.com>
2021-06-02 22:50:36 +0200

    Register services in IoC.

--- src/Modules/Conferences/Confab.Modules.Conferences.Core/Extensions.cs
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Extensions.cs
@@ -1,4 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
+using Confab.Modules.Conferences.Core.Policies;
+using Confab.Modules.Conferences.Core.Repositories;
+using Confab.Modules.Conferences.Core.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: InternalsVisibleTo(assemblyName: "Confab.Modules.Conferences.Api")]
@@ -8,6 +11,10 @@ namespace Confab.Modules.Conferences.Core
     {
         public static IServiceCollection AddCore(this IServiceCollection services)
         {
+            services.AddSingleton<IHostRepository, InMemoryHostRepository>();
+            services.AddSingleton<IConferenceDeletionPolice, ConferenceDeletionPolice>();
+            services.AddSingleton<IHostDeletionPolice, HostDeletionPolice>();
+            services.AddScoped<IHostService, HostService>();
             return services;
         }
     }
