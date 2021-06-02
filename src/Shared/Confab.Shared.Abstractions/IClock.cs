0dd9a60712309573af38882c72b814e88569ad28
TeoVincent <artur.wincenciak@gmail.com>
2021-06-02 22:14:29 +0200

    Impl own wrapper for DateTime.UtcNow by IClock, UtcClock.

---
+++ src/Shared/Confab.Shared.Abstractions/IClock.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Confab.Shared.Abstractions
+{
+    public interface IClock
+    {
+        DateTime CurrentDate();
+    }
+}
---
+++ src/Shared/Confab.Shared.Infrastructure/Time/UtcClock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Confab.Shared.Abstractions;
+
+namespace Confab.Shared.Infrastructure.Time
+{
+    class UtcClock : IClock
+    {
+        public DateTime CurrentDate() => DateTime.UtcNow;
+    }
+}
