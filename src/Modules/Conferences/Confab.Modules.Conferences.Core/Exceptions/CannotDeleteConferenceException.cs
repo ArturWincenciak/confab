39a945d0a1564a1277e95c3ce47787e71999e827
TeoVincent <artur.wincenciak@gmail.com>
2021-06-03 19:40:30 +0200

    Add custom exception of conferences part.

---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Exceptions/CannotDeleteConferenceException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Conferences.Core.Exceptions
+{
+    internal class CannotDeleteConferenceException : ConfabException
+    {
+        public Guid Id { get; }
+
+        public CannotDeleteConferenceException(Guid id)
+            : base($"Conference with ID: '{id}' cannot be deleted.")
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Exceptions/ConferenceNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Conferences.Core.Exceptions
+{
+    internal class ConferenceNotFoundException : ConfabException
+    {
+        public Guid Id { get; }
+
+        public ConferenceNotFoundException(Guid id) 
+            : base($"Conference with ID: '{id}' was not found.")
+        {
+            Id = id;
+        }
+    }
+}
