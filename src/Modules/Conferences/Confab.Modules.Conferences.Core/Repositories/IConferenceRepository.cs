e8a282e76499b39e78d4c83b514b69a759bdfd8c
TeoVincent <artur.wincenciak@gmail.com>
2021-06-03 19:40:08 +0200

    Impl service interface and repository interface of conferences part.

---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Repositories/IConferenceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Confab.Modules.Conferences.Core.Entities;
+
+namespace Confab.Modules.Conferences.Core.Repositories
+{
+    internal interface IConferenceRepository
+    {
+        Task AddAsync(Conference conference);
+        Task<Conference> GetAsync(Guid id);
+        Task<IReadOnlyList<Conference>> BrowseAsync();
+        Task UpdateAsync(Conference conference);
+        Task DeleteAsync(Conference conference);
+    }
+}
