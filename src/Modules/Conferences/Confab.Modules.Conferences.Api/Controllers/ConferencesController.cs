ef262352a4530eccfa0f1e5c347ec5d3bb52f9a3
TeoVincent <artur.wincenciak@gmail.com>
2021-06-04 14:50:35 +0200

    Impl API controller of conferences.

---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Api/Controllers/ConferencesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Confab.Modules.Conferences.Core.DTO;
+using Confab.Modules.Conferences.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Confab.Modules.Conferences.Api.Controllers
+{
+    internal class ConferencesController : ConferencesControllerBase
+    {
+        private readonly IConferenceService _conferenceService;
+
+        public ConferencesController(IConferenceService conferenceService)
+        {
+            _conferenceService = conferenceService;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ConferenceDetailsDto>> GetAsync(Guid id) =>
+            OkOrNotFound(await _conferenceService.GetAsync(id));
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<ConferenceDto>>> BrowseAsync() =>
+            Ok(await _conferenceService.BrowseAsync());
+
+        [HttpPost]
+        public async Task<ActionResult> AddAsync(ConferenceDetailsDto dto)
+        {
+            await _conferenceService.AddAsync(dto);
+            return CreatedAtAction("Get", new {id = dto.Id}, null);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult> UpdateAsync(Guid id, ConferenceDetailsDto dto)
+        {
+            dto.Id = id;
+            await _conferenceService.UpdateAsync(dto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> DeleteAsync(Guid id)
+        {
+            await _conferenceService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
