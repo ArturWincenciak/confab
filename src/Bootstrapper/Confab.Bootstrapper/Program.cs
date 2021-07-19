23bc9fba431894a802f996e2f57706b5c46f825f
TeoVincent <artur.wincenciak@gmail.com>
2021-07-19 15:54:16 +0200

    Talk about what you do at application startup...

--- src/Bootstrapper/Confab.Bootstrapper/Program.cs
+++ src/Bootstrapper/Confab.Bootstrapper/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 using Confab.Shared.Infrastructure.Modules;
@@ -7,17 +8,45 @@ namespace Confab.Bootstrapper
 {
     public class Program
     {
-        public static async Task Main(string[] args) =>
-            await CreateHostBuilder(args)
-                .Build()
-                .RunAsync();
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("\n---\nStarting... creating host builder...\n---\n");
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureModules();
+            var builder = CreateHostBuilder(args);
+
+            Console.WriteLine("\n---\nHost builder has been created..., building the host...\n---\n");
+
+            var host = builder.Build();
+
+            Console.WriteLine("\n---\nHost has been built..., running...\n---\n");
+
+            await host.RunAsync();
+
+            Console.WriteLine("\n---\nHost has been run.");
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            Console.WriteLine("\n---\nStarting create host builder method..., creating default builder...");
+
+            var defaultBuilder = Host.CreateDefaultBuilder(args);
+
+            Console.WriteLine("\n---\nDefault host builder has been created..., configuring web host defaults...");
+
+            var configureWebHostDefaults = defaultBuilder.ConfigureWebHostDefaults(webBuilder =>
+            {
+                Console.WriteLine("\n---\nRunning lambda with setting Startup class " +
+                                  "as entry point on host builder...\n---\n");
+                webBuilder.UseStartup<Startup>();
+            });
+
+            Console.WriteLine("\n---\nWeb host defaults has been configured..., configuring modules...\n---\n");
+
+            var configureModules = configureWebHostDefaults.ConfigureModules();
+
+            Console.WriteLine("\n---\nModules has been configured..., returning the host builder...\n---\n");
+
+            return configureModules;
+        }
     }
 }
