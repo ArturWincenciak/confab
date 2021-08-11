e9e6f26363c91d60615f57f07a887f9090641d2e
TeoVincent <teo.vincent.com@gmail.com>
2021-08-11 15:38:00 +0200

    Speaker DB context configuration

---
+++ src/Modules/Speakers/Confab.Modules.Speakers.Core/DAL/Configurations/SpeakerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Confab.Modules.Speakers.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Confab.Modules.Speakers.Core.DAL.Configurations
+{
+    internal class SpeakerConfiguration : IEntityTypeConfiguration<Speaker>
+    {
+        public void Configure(EntityTypeBuilder<Speaker> builder)
+        {
+        }
+    }
+}
