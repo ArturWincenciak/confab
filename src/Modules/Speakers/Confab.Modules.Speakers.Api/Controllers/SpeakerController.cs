1413ade8ae819a056f72374ec6a9b57b6d28eec7
TeoVincent <teo.vincent.com@gmail.com>
2021-08-11 15:30:10 +0200

    Speakers controller

---
+++ src/Modules/Speakers/Confab.Modules.Speakers.Api/Controllers/SpeakerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Confab.Modules.Speakers.Core.DTO;
+using Confab.Modules.Speakers.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Confab.Modules.Speakers.Api.Controllers
+{
+    internal class SpeakerController : SpeakerControllerBase
+    {
+        private readonly ISpeakerService _service;
+
+        public SpeakerController(ISpeakerService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<SpeakerDto>> GetAsync(Guid id) =>
+            OkOrNotFound(await _service.GetAsync(id));
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<SpeakerDto>>> BrowseAsync() =>
+            Ok(await _service.BrowseAsync());
+
+        [HttpPost]
+        public async Task<ActionResult> AddAsync(SpeakerDto dto)
+        {
+            await _service.AddAsync(dto);
+            return CreatedAtAction("Get", new {id = dto.Id}, null);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult> UpdateAsync(Guid id, SpeakerDto dto)
+        {
+            dto.Id = id;
+            await _service.UpdateAsync(dto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> DeleteAsync(Guid id)
+        {
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ src/Modules/Speakers/Confab.Modules.Speakers.Api/Controllers/SpeakerControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Confab.Modules.Speakers.Api.Controllers
+{
+    [ApiController]
+    [Route(SpeakersModule.BasePath + "/" + "[controller]")]
+    internal class SpeakerControllerBase : ControllerBase
+    {
+        protected ActionResult<T> OkOrNotFound<T>(T model)
+        {
+            if (model is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+    }
+}
