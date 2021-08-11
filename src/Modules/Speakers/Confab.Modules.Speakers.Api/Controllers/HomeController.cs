5c23a40cd091f8771cf1c0cdaabcfe3f8c329f8a
TeoVincent <teo.vincent.com@gmail.com>
2021-08-11 20:04:45 +0200

    Speakers module home controller

---
+++ src/Modules/Speakers/Confab.Modules.Speakers.Api/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Confab.Modules.Speakers.Api.Controllers
+{
+    [Route(SpeakersModule.BasePath)]
+    internal class HomeController : SpeakerControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Speakers API";
+    }
+}
