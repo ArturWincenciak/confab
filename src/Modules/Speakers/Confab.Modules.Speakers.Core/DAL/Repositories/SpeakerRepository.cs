95fbb6c01c44b0e556bd5cf7e4ef2a6150b81c3b
TeoVincent <artur.wincenciak@gmail.com>
2021-08-13 22:16:17 +0200

    Do not use async/await where it is not needed

--- src/Modules/Speakers/Confab.Modules.Speakers.Core/DAL/Repositories/SpeakerRepository.cs
+++ src/Modules/Speakers/Confab.Modules.Speakers.Core/DAL/Repositories/SpeakerRepository.cs
@@ -25,9 +25,9 @@ namespace Confab.Modules.Speakers.Core.DAL.Repositories
             await _dbContext.SaveChangesAsync();
         }
 
-        public async Task<bool> ExistsAsync(Guid id)
+        public Task<bool> ExistsAsync(Guid id)
         {
-            return await _speakers.AnyAsync(s => s.Id == id);
+            return _speakers.AnyAsync(s => s.Id == id);
         }
 
         public Task<Speaker> GetAsync(Guid id)
