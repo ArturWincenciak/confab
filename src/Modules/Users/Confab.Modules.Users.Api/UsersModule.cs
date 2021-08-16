cbc572c75fb0ee49233605f77cbf185c349a9364
TeoVincent <artur.wincenciak@gmail.com>
2021-08-16 12:32:27 +0200

    Users module - new empty template of module

---
+++ src/Modules/Users/Confab.Modules.Users.Api/UsersModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Confab.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Modules.Users.Api
+{
+    public class UsersModule : IModule
+    {
+        public string Name { get; }
+        public string Path { get; }
+        public IEnumerable<string> Policies { get; }
+        public void Register(IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Modules/Users/Confab.Modules.Users.Core/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo(assemblyName: "Confab.Modules.Users.Api")]
+namespace Confab.Modules.Users.Core
+{
+    internal static class Extensions
+    {
+    }
+}
