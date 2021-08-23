85c7bc1d4d741c694f10f5b1cc129df790825fe5
TeoVincent <artur.wincenciak@gmail.com>
2021-08-23 13:32:21 +0200

    Define JSON content type attr for all endpoints

--- src/Modules/Conferences/Confab.Modules.Conferences.Api/Controllers/ConferencesControllerBase.cs
+++ src/Modules/Conferences/Confab.Modules.Conferences.Api/Controllers/ConferencesControllerBase.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Confab.Shared.Infrastructure.Api;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Confab.Modules.Conferences.Api.Controllers
 {
     [ApiController]
     [Route(ConferencesModule.BasePath + "/" + "[controller]")]
+    [ProducesDefaultContentType]
     internal class ConferencesControllerBase : ControllerBase
     {
         protected ActionResult<T> OkOrNotFound<T>(T model)
