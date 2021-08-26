650a9c7b1516fa08da58e8454bcbcf30611c8a6a
TeoVincent <artur.wincenciak@gmail.com>
2021-08-26 14:29:28 +0200

    Entities of Ticket module

---
+++ src/Modules/Tickets/Confab.Modules.Tickets.Core/Exceptions/TicketAlreadyPurchasedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Confab.Modules.Tickets.Core.Exceptions
+{
+    internal class TicketAlreadyPurchasedException : Exception
+    {
+        public TicketAlreadyPurchasedException(Guid conferenceId, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
