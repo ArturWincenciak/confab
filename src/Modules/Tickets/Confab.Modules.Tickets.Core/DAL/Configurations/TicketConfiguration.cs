c84add772f544904565d41806acb8ffa81075ee3
TeoVincent <artur.wincenciak@gmail.com>
2021-09-03 15:18:09 +0200

    Migration and update db schema in Tickets module

---
+++ src/Modules/Tickets/Confab.Modules.Tickets.Core/DAL/Configurations/TicketConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Confab.Modules.Tickets.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Confab.Modules.Tickets.Core.DAL.Configurations
+{
+    internal class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+        }
+    }
+}
