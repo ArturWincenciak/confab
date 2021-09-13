11fe91552104e154bb70f9bc975d41a8eadecd38
TeoVincent <artur.wincenciak@gmail.com>
2021-09-13 15:22:56 +0200

    Command dispatcher in infra

---
+++ src/Shared/Confab.Shared.Infrastructure/Commands/CommandDispatcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confab.Shared.Abstractions.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Confab.Shared.Infrastructure.Commands
+{
+    internal sealed class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<CommandDispatcher> _logger;
+
+        public CommandDispatcher(IServiceProvider serviceProvider, ILogger<CommandDispatcher> logger)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+
+        public async Task SendAsync<TCommand>(TCommand command) where TCommand : class, ICommand
+        {
+            try
+            {
+                if (command is null)
+                {
+                    _logger.LogWarning("Command is null. Skipping call handler for the command.");
+                    return;
+                }
+
+                using var scope = _serviceProvider.CreateScope();
+                var handler = _serviceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+                await handler.HandleAsync(command);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"{ex.Message}; Command: '{command}'.");
+                throw;
+            }
+        }
+    }
+}
