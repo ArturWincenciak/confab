0a58dbce76d09300dc376afc63cdc000ecd69bdf
TeoVincent <artur.wincenciak@gmail.com>
2021-09-13 13:14:08 +0200

    CR: Move record definitions to its own files

--- src/Modules/Agendas/Confab.Modules.Agendas.Domain/Submissions/Events/SubmissionAdded.cs
+++ src/Modules/Agendas/Confab.Modules.Agendas.Domain/Submissions/Events/SubmissionAdded.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using Confab.Modules.Agendas.Domain.Submissions.Entities;
+﻿using Confab.Modules.Agendas.Domain.Submissions.Entities;
 using Confab.Shared.Abstractions.Kernel;
 
 namespace Confab.Modules.Agendas.Domain.Submissions.Events
 {
     internal record SubmissionAdded(Submission Submission) : IDomainEvent;
-
-    internal record SubmissionApproved(Guid Id) : IDomainEvent;
-
-    internal record SubmissionRejected(Guid Id) : IDomainEvent;
 }
---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Domain/Submissions/Events/SubmissionRejected.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using Confab.Shared.Abstractions.Kernel;
+
+namespace Confab.Modules.Agendas.Domain.Submissions.Events
+{
+    internal record SubmissionRejected(Guid Id) : IDomainEvent;
+}
