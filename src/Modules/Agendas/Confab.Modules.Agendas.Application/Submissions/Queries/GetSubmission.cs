41f452869505e83544a9a83ec3a3e8286a29bcf6
TeoVincent <artur.wincenciak@gmail.com>
2021-09-14 17:17:48 +0200

    Get submission query definition with dtos

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Submissions/Queries/GetSubmission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Confab.Shared.Abstractions.Queries;
+
+namespace Confab.Modules.Agendas.Application.Submissions.Queries
+{
+    public sealed record GetSubmission(Guid Id) : IQuery<GetSubmission.SubmissionDto>
+    {
+        internal sealed record SubmissionDto(Guid Id, Guid ConferenceId, string Title, string Description, int Level,
+            string Status, IEnumerable<string> Tags, IEnumerable<SpeakerDto> Speakers) : IQueryResult;
+
+        internal sealed record SpeakerDto(Guid Id, string FullName);
+    }
+}
