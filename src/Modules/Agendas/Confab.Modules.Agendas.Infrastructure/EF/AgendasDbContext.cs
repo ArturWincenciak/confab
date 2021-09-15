73d077a1058487571ca6abe44f6a123fcfaa1aa3
TeoVincent <artur.wincenciak@gmail.com>
2021-09-15 14:59:52 +0200

    Agendas DB context

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Infrastructure/EF/AgendasDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Confab.Modules.Agendas.Domain.Submissions.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Confab.Modules.Agendas.Infrastructure.EF
+{
+    internal sealed class AgendasDbContext : DbContext
+    {
+        public DbSet<Submission> Submissions { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+
+        public AgendasDbContext(DbContextOptions<AgendasDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("agendas");
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
