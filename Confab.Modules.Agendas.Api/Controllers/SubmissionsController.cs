f3ff92000a8b46e0c0477f25a8d96749ffe1213f
TeoVincent <artur.wincenciak@gmail.com>
2021-09-18 14:17:12 +0200

    Revert __PRACTICE__HOW__HOW__NOT_FOUND__DOES__WORK__

--- Confab.Modules.Agendas.Api/Controllers/SubmissionsController.cs
+++ Confab.Modules.Agendas.Api/Controllers/SubmissionsController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Policy;
 using System.Threading.Tasks;
 using Confab.Modules.Agendas.Application.Submissions.Commands;
 using Confab.Modules.Agendas.Application.Submissions.Queries;
@@ -9,8 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Confab.Modules.Agendas.Api.Controllers
 {
-    internal sealed record TestType(int Age, string Name);
-
     internal class SubmissionsController : AgendasControllerBase
     {
         private readonly ICommandDispatcher _commandDispatcher;
@@ -21,142 +18,6 @@ namespace Confab.Modules.Agendas.Api.Controllers
             _commandDispatcher = commandDispatcher;
             _queryDispatcher = queryDispatcher;
         }
-
-
-        [HttpGet("test11")]
-        public ActionResult<object> GetTest11()
-        {
-            var result = new TestType(36, "Arthur");
-            return NotFound(result);
-        }
-
-        [HttpGet("test111")]
-        public ActionResult<TestType> GetTest111()
-        {
-            var result = new TestType(36, "Arthur");
-            return NotFound(result);
-        }
-
-        [HttpGet("test12")]
-        public ActionResult<object> GetTest12()
-        {
-            return NotFound(new {Id = "this is the id"});
-        }
-
-        [HttpGet("test122")]
-        public ActionResult<TestType> GetTest122()
-        {
-            return NotFound(new {Id = "this is the id"});
-        }
-
-        [HttpGet("test13")]
-        public ActionResult<object> GetTest13()
-        {
-            return NotFound();
-        }
-
-        [HttpGet("test133")]
-        public ActionResult<TestType> GetTest133()
-        {
-            return NotFound();
-        }
-
-        [HttpGet("test14")]
-        public ActionResult<object> GetTest14()
-        {
-            var resultDto = new TestType(36, "Arthur");
-            var result = TestRetValue1(resultDto);
-            return result;
-        }
-
-        [HttpGet("test14a")]
-        public ActionResult<object> GetTest14a()
-        {
-            NotFoundResult nf = NotFound();
-            ActionResult<TestType> result = nf;
-            ActionResult<object> obj = result;
-            return obj;
-        }
-
-        [HttpGet("test14b")]
-        public ActionResult<object> GetTest14b()
-        {
-            NotFoundResult nf = NotFound();
-            ActionResult<object> obj = nf;
-            return obj;
-        }
-
-        [HttpGet("test144")]
-        public ActionResult<TestType> GetTest144()
-        {
-            var resultDto = new TestType(36, "Arthur");
-            var result = TestRetValue1(resultDto);
-            return result;
-        }
-
-        [HttpGet("test144a")]
-        public ActionResult<TestType> GetTest144a()
-        {
-            NotFoundResult nf = NotFound();
-            ActionResult<TestType> result = nf;
-            return result;
-        }
-
-        private ActionResult<T> TestRetValue1<T>(T resultDto)
-        {
-            var nf = NotFound();
-            ActionResult<T> res = nf;
-            return res;
-        }
-
-
-        [HttpGet("test2")]
-        public ActionResult<object> GetTest2()
-        {
-            var resultDto = new TestType(36, "Arthur");
-            var result = TestRetValue2(resultDto);
-            return result;
-        }
-
-        private ActionResult<object> TestRetValue2<T>(T resultDto)
-        {
-            var nf = NotFound();
-            ActionResult<T> res = nf;
-            return res;
-        }
-
-
-        [HttpGet("test3")]
-        public ActionResult<object> GetTest3()
-        {
-            var resultDto = new TestType(36, "Arthur");
-            var result = TestRetValue3(resultDto);
-            return result;
-        }
-
-        private ActionResult<object> TestRetValue3<T>(T resultDto)
-        {
-            var nf = NotFound();
-            ActionResult<object> res = nf;
-            return res;
-        }
-
-
-        [HttpGet("test4")]
-        public ActionResult<object> GetTest4()
-        {
-            var resultDto = new TestType(36, "Arthur");
-            var result = TestRetValue4(resultDto);
-            return result;
-        }
-
-        private ActionResult<object> TestRetValue4<T>(T resultDto)
-        {
-            var nf = NotFound();
-            ActionResult res = nf;
-            return res;
-        }
-
 
         [HttpGet("{id:guid}")]
         public async Task<ActionResult<object>> GetAsync(Guid id)
