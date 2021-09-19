6a3379ed400d66f351ce160a7350d168b6f32071
TeoVincent <artur.wincenciak@gmail.com>
2021-09-19 19:53:40 +0200

    Corrent in TEMP version get subbmission handler

--- src/Modules/Agendas/Confab.Modules.Agendas.Infrastructure/EF/Queries/Handlers/GetSubmissionHandler.cs
+++ src/Modules/Agendas/Confab.Modules.Agendas.Infrastructure/EF/Queries/Handlers/GetSubmissionHandler.cs
@@ -19,31 +19,16 @@ namespace Confab.Modules.Agendas.Infrastructure.EF.Queries.Handlers
 
         public async Task<GetSubmission.SubmissionDto> HandleAsync(GetSubmission query)
         {
-            //var submission = await _submissions
-            //    .AsNoTracking()
-            //    .Where(x => x.Id == query.Id) // it work correctly! why?
-            //    //.Where(x => x.Id.Equals(query.Id)) // it not work!
-            //    .Include(x => x.Speakers)
-            //    .Select(x => Map(x))
-            //    .SingleOrDefaultAsync();
+            var submission = await _submissions
+                .AsNoTracking()
+                .Where(x => x.Id == query.Id) // it work correctly! why?
+                //.Where(x => x.Id.Equals(query.Id)) // it not work!
+                .Include(x => x.Speakers)
+                .Select(x => Map(x))
+                .SingleOrDefaultAsync();
 
-            var sub = Submission.Create(Guid.NewGuid(), "x", "y", 1, new[] {"z"}, new Speaker[] {null});
-
-            if (sub.Id == query.Id)
-                Console.WriteLine("Ognia");
-
-            if(query.Id == sub.Id)
-                Console.WriteLine("Ognia");
-
-            if (sub.Id.Equals(query.Id))
-                Console.WriteLine("Ognia");
-
-            if (query.Id.Equals(sub.Id))
-                Console.WriteLine("Odnia");
-
-            return null;
-            //return submission;
-            //.FirstOrDefaultAsync();
+            //return null;
+            return submission;
         }
 
         private static GetSubmission.SubmissionDto Map(Submission entity)
