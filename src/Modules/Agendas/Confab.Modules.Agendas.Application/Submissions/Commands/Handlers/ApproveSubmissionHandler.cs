c62ba65647d96a90a3570af3761a99d744ce7347
TeoVincent <artur.wincenciak@gmail.com>
2021-09-22 15:00:40 +0200

    Publish domaind events as integration events

--- src/Modules/Agendas/Confab.Modules.Agendas.Application/Submissions/Commands/Handlers/ApproveSubmissionHandler.cs
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Submissions/Commands/Handlers/ApproveSubmissionHandler.cs
@@ -1,9 +1,11 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Confab.Modules.Agendas.Application.Submissions.Exceptions;
+using Confab.Modules.Agendas.Application.Submissions.Services;
 using Confab.Modules.Agendas.Domain.Submissions.Repositories;
 using Confab.Shared.Abstractions.Commands;
 using Confab.Shared.Abstractions.Kernel;
+using Confab.Shared.Abstractions.Messaging;
 
 namespace Confab.Modules.Agendas.Application.Submissions.Commands.Handlers
 {
@@ -11,12 +13,16 @@ namespace Confab.Modules.Agendas.Application.Submissions.Commands.Handlers
     {
         private readonly IDomainEventDispatcher _domainEventDispatcher;
         private readonly ISubmissionRepository _submissionRepository;
+        private readonly IMessageBroker _messageBroker;
+        private readonly IMessageMapper _messageMapper;
 
         public ApproveSubmissionHandler(ISubmissionRepository submissionRepository,
-            IDomainEventDispatcher domainEventDispatcher)
+            IDomainEventDispatcher domainEventDispatcher, IMessageBroker messageBroker, IMessageMapper messageMapper)
         {
             _submissionRepository = submissionRepository;
             _domainEventDispatcher = domainEventDispatcher;
+            _messageBroker = messageBroker;
+            _messageMapper = messageMapper;
         }
 
         public async Task HandleAsync(ApproveSubmission command)
@@ -27,7 +33,10 @@ namespace Confab.Modules.Agendas.Application.Submissions.Commands.Handlers
 
             submission.Approve();
             await _submissionRepository.UpdateAsync(submission);
+
             await _domainEventDispatcher.SendAsync(submission.Events.ToArray());
+            var integrationEvents = _messageMapper.Map(submission.Events);
+            await _messageBroker.PublishAsync(integrationEvents.ToArray());
         }
     }
 }
--- src/Modules/Agendas/Confab.Modules.Agendas.Application/Submissions/Commands/Handlers/CreateSubmissionHandler.cs
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Submissions/Commands/Handlers/CreateSubmissionHandler.cs
@@ -1,41 +1,50 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Confab.Modules.Agendas.Application.Submissions.Exceptions;
+using Confab.Modules.Agendas.Application.Submissions.Services;
 using Confab.Modules.Agendas.Domain.Submissions.Entities;
 using Confab.Modules.Agendas.Domain.Submissions.Repositories;
 using Confab.Shared.Abstractions.Commands;
 using Confab.Shared.Abstractions.Kernel;
 using Confab.Shared.Abstractions.Kernel.Types;
+using Confab.Shared.Abstractions.Messaging;
 
 namespace Confab.Modules.Agendas.Application.Submissions.Commands.Handlers
 {
     internal sealed class CreateSubmissionHandler : ICommandHandler<CreateSubmission, CreateSubmission.SubmissionId>
     {
         private readonly IDomainEventDispatcher _domainEventDispatcher;
+        private readonly IMessageBroker _messageBroker;
+        private readonly IMessageMapper _messageMapper;
         private readonly ISpeakerRepository _speakerRepository;
         private readonly ISubmissionRepository _submissionRepository;
 
         public CreateSubmissionHandler(ISubmissionRepository submissionRepository, ISpeakerRepository speakerRepository,
-            IDomainEventDispatcher domainEventDispatcher)
+            IDomainEventDispatcher domainEventDispatcher, IMessageBroker messageBroker, IMessageMapper messageMapper)
         {
             _submissionRepository = submissionRepository;
             _speakerRepository = speakerRepository;
             _domainEventDispatcher = domainEventDispatcher;
+            _messageBroker = messageBroker;
+            _messageMapper = messageMapper;
         }
 
-        public async Task<CreateSubmission.SubmissionId> HandleAsync(CreateSubmission command)
+        public async Task<CreateSubmission.SubmissionId> HandleAsync(CreateSubmission cmd)
         {
-            var speakerIds = command.SpeakerIds.Select(x => new AggregateId(x));
+            var speakerIds = cmd.SpeakerIds.Select(x => new AggregateId(x));
             var speakers = await _speakerRepository.BrowseAsync(speakerIds);
 
-            var allSpeakerExists = speakers.Count() == command.SpeakerIds.Count();
+            var allSpeakerExists = speakers.Count() == cmd.SpeakerIds.Count();
             if (!allSpeakerExists)
-                throw new NotAllSpeakersExistsException(command.SpeakerIds, speakers.Select(x => x.Id.Value));
+                throw new NotAllSpeakersExistsException(cmd.SpeakerIds, speakers.Select(x => x.Id.Value));
 
-            var submission = Submission.Create(command.ConferenceId, command.Title, command.Title, command.Level,
-                command.Tags, speakers);
+            var submission = Submission.Create(cmd.ConferenceId, cmd.Title, cmd.Title, cmd.Level, cmd.Tags, speakers);
             await _submissionRepository.AddAsync(submission);
+
             await _domainEventDispatcher.SendAsync(submission.Events.ToArray());
+            var integrationEvents = _messageMapper.Map(submission.Events);
+            await _messageBroker.PublishAsync(integrationEvents.ToArray());
+
             return new CreateSubmission.SubmissionId(submission.Id);
         }
     }
