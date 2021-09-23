fe50f3aa9ea8a830a96bf1bdca39b089f8f7d0e9
TeoVincent <artur.wincenciak@gmail.com>
2021-09-23 15:22:45 +0200

    Call for paper aggregate root

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Domain/CallForPaper/Entities/CallForPapers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Confab.Modules.Agendas.Domain.CallForPaper.Exceptions;
+using Confab.Shared.Abstractions.Kernel.Types;
+
+namespace Confab.Modules.Agendas.Domain.CallForPaper.Entities
+{
+    public sealed class CallForPapers : AggregateRoot
+    {
+        public ConferenceId ConferenceId { get; private set; }
+        public DateTime From { get; private set; }
+        public DateTime To { get; private set; }
+        public bool IsOpened { get; private set; }
+
+        private CallForPapers(AggregateId id) => Id = id;
+
+        private CallForPapers(AggregateId id, ConferenceId conferenceId, DateTime from, DateTime to, bool isOpened, int version = 0)
+        {
+            Id = id;
+            ConferenceId = conferenceId;
+            From = from;
+            To = to;
+            IsOpened = isOpened;
+            Version = version;
+        }
+
+        public static CallForPapers Create(Guid conferenceId, DateTime from, DateTime to)
+        {
+            var id = Guid.NewGuid();
+            var callForPaper = new CallForPapers(id);
+            callForPaper.ConferenceId = conferenceId;
+            callForPaper.ChangeDateRange(from, to);
+            callForPaper.IsOpened = false;
+            callForPaper.ClearEvents();
+            return callForPaper;
+        }
+
+        public void ChangeDateRange(DateTime @from, DateTime to)
+        {
+            if (from.Date > to.Date)
+                throw new InvalidCallForPapersDateException(from, to);
+
+            From = from;
+            To = to;
+            IncrementVersion();
+        }
+
+        public void Open()
+        {
+            IsOpened = true;
+            IncrementVersion();
+        }
+
+        public void Close()
+        {
+            IsOpened = false;
+            IncrementVersion();
+        }
+    }
+}
---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Domain/CallForPaper/Exceptions/InvalidCallForPapersDateException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Agendas.Domain.CallForPaper.Exceptions
+{
+    internal class InvalidCallForPapersDateException : ConfabException
+    {
+        public DateTime From { get; }
+        public DateTime To { get; }
+
+        public InvalidCallForPapersDateException(DateTime from, DateTime to)
+            : base($"CFP has invalid dates, from: '{from}', to: '{to}'.")
+        {
+            From = @from;
+            To = to;
+        }
+    }
+}
