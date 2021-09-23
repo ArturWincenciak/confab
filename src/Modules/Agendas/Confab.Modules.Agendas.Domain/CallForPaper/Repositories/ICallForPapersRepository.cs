85425d058c6b87c69bcc4dabac11f0a9a37e4bbf
TeoVincent <artur.wincenciak@gmail.com>
2021-09-23 15:52:15 +0200

    Call for papers repository interface

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Domain/CallForPaper/Repositories/ICallForPapersRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Confab.Modules.Agendas.Domain.CallForPaper.Entities;
+using Confab.Shared.Abstractions.Kernel.Types;
+
+namespace Confab.Modules.Agendas.Domain.CallForPaper.Repositories
+{
+    public interface ICallForPapersRepository
+    {
+        Task<CallForPapers> GetAsync(ConferenceId conferenceId);
+        Task<bool> ExistsAsync(ConferenceId conferenceId);
+        Task AddAsync(CallForPapers entity);
+        Task UpdateAsync(CallForPapers entity);
+    }
+}
