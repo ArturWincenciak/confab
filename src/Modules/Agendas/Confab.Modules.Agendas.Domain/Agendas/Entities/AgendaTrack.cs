974f5d9f0700ca49802b915956c8a7210094a841
TeoVincent <artur.wincenciak@gmail.com>
2021-09-25 14:51:42 +0200

    Agenda Track aggregate root in Agendas modules

--- src/Modules/Agendas/Confab.Modules.Agendas.Domain/Agendas/Entities/AgendaTrack.cs
+++ src/Modules/Agendas/Confab.Modules.Agendas.Domain/Agendas/Entities/AgendaTrack.cs
@@ -1,9 +1,32 @@
-﻿using Confab.Shared.Abstractions.Kernel.Types;
+﻿using System;
+using System.Collections.Generic;
+using Confab.Shared.Abstractions.Kernel.Types;
 using Confab.Shared.Abstractions.Kernel.Types.Base;
 
 namespace Confab.Modules.Agendas.Domain.Agendas.Entities
 {
     public class AgendaTrack : AggregateRoot
     {
+        public ConferenceId ConferenceId { get; private set; }
+        public string Name { get; private set; }
+        public ICollection<AgendaSlot> Slots { get; }
+
+        public static AgendaTrack Create(ConferenceId conferenceId, string name)
+        {
+            var entity = new AgendaTrack
+            {
+                Id = Guid.NewGuid(),
+                ConferenceId = conferenceId
+            };
+            entity.ChangeName(name);
+            entity.ClearEvents();
+
+            return entity;
+        }
+
+        private void ChangeName(string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
