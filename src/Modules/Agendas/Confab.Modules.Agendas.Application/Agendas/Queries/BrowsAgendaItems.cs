c6e95740e00ed5ad3b7c8672faa19ee609307a8f
TeoVincent <artur.wincenciak@gmail.com>
2021-09-26 14:39:20 +0200

    Queries in Agendas submodules

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Queries/BrowsAgendaItems.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Confab.Shared.Abstractions.Queries;
+
+namespace Confab.Modules.Agendas.Application.Agendas.Queries
+{
+    public sealed record BrowsAgendaItems() : IQuery<BrowsAgendaItems.AgendaItemsDto>
+    {
+        //todo: sprawdz czy deserializator ogarnie ze to jest IEnumerable i zwróci kolekcę :)
+        public class AgendaItemsDto : IEnumerable<BrowsAgendaItems.AgendaItemDto>, IQueryResult
+        {
+            private readonly IEnumerable<AgendaItemDto> _agendaItem;
+
+            public AgendaItemsDto(IEnumerable<AgendaItemDto> agendaItem)
+            {
+                _agendaItem = agendaItem;
+            }
+
+            public IEnumerator<AgendaItemDto> GetEnumerator()
+            {
+                return _agendaItem.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        public sealed record AgendaItemDto(Guid Id, Guid ConferenceId, string Title, string Description, int Level,
+            IEnumerable<string> Tags, IEnumerable<SpeakerDto> Speakers);
+
+        public sealed record SpeakerDto(Guid Id, string FullName);
+
+    };
+
+}
