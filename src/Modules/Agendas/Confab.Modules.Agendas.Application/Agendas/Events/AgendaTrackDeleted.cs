3ada7674cc0d99da42288430dc231fb1fb172500
TeoVincent <artur.wincenciak@gmail.com>
2021-09-26 11:34:27 +0200

    Integration events in Agenda submodules

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Events/AgendaTrackDeleted.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using Confab.Shared.Abstractions.Events;
+
+namespace Confab.Modules.Agendas.Application.Agendas.Events
+{
+    public sealed record AgendaTrackDeleted(Guid Id) : IEvent;
+}
---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Events/AgendaTrackUpdated.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using Confab.Shared.Abstractions.Events;
+
+namespace Confab.Modules.Agendas.Application.Agendas.Events
+{
+    public sealed record AgendaTrackUpdated(Guid Id) : IEvent;
+}
