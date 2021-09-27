7fcf21c9c4b82b08d95c2eeafb96fc510a983aba
TeoVincent <artur.wincenciak@gmail.com>
2021-09-27 16:18:07 +0200

    Static AgendaSlotType

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Types/AgendaSlotType.cs
@@ -0,0 +1,21 @@
+﻿using Confab.Modules.Agendas.Application.Agendas.Exceptions;
+using Confab.Modules.Agendas.Domain.Agendas.Entities;
+
+namespace Confab.Modules.Agendas.Application.Agendas.Types
+{
+    internal static class AgendaSlotType
+    {
+        public const string Regular = "regular";
+        public const string Placeholder = "placeholder";
+
+        public static string GetSlotType(object slot)
+        {
+            return slot switch
+            {
+                RegularAgendaSlot => Regular,
+                PlaceholderAgendaSlot => Placeholder,
+                _ => throw new AgendaSlotTypeNotFoundException(slot.GetType().Name)
+            };
+        }
+    }
+}
