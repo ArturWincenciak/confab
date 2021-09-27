820ac04145b8ca1574453959ba1012ddd1771098
TeoVincent <artur.wincenciak@gmail.com>
2021-09-27 16:33:12 +0200

    WIP AssignPlaceholderAgendaSlotHandler of Agendas

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Commands/Handlers/AssignPlaceholderAgendaSlotHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Confab.Shared.Abstractions.Commands;
+
+namespace Confab.Modules.Agendas.Application.Agendas.Commands.Handlers
+{
+    internal sealed class AssignPlaceholderAgendaSlotHandler : ICommandHandler<AssignPlaceholderAgendaSlot>
+    {
+        public Task HandleAsync(AssignPlaceholderAgendaSlot command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
