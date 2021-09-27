e17c587d18a9c452f3260ef06cf3ab50c48bc4bd
TeoVincent <artur.wincenciak@gmail.com>
2021-09-27 16:12:11 +0200

    Exceptions of Agendas submodules

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Exceptions/AgendaSlotTypeNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Agendas.Application.Agendas.Exceptions
+{
+    internal sealed class AgendaSlotTypeNotFoundException : ConfabException
+    {
+        public string Type { get; }
+
+        public AgendaSlotTypeNotFoundException(string type)
+            : base($"Agenda slot type: '{type}' was not found.")
+        {
+            Type = type;
+        }
+    }
+}
---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Exceptions/AgendaTrackAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Agendas.Application.Agendas.Exceptions
+{
+    internal sealed class AgendaTrackAlreadyExistsException : ConfabException
+    {
+        public AgendaTrackAlreadyExistsException(Guid agendaTrackId)
+            : base($"Agenda track with ID: '{agendaTrackId}' already exists.")
+        {
+        }
+    }
+}
