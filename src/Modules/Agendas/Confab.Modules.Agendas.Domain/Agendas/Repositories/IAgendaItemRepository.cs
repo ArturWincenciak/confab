771a4b52e7b065f8bee74d7f9b2afff4f2b7775b
TeoVincent <artur.wincenciak@gmail.com>
2021-09-27 17:06:46 +0200

    IAgendaItemRepository with SpeakerId

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Domain/Agendas/Repositories/IAgendaItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Confab.Modules.Agendas.Domain.Agendas.Entities;
+using Confab.Shared.Abstractions.Kernel.Types;
+using Confab.Shared.Abstractions.Kernel.Types.Base;
+
+namespace Confab.Modules.Agendas.Domain.Agendas.Repositories
+{
+    public interface IAgendaItemRepository
+    {
+        Task<IEnumerable<AgendaItem>> BrowsAsync(IEnumerable<SpeakerId> speakerIds);
+        Task<AgendaItem> GetAsync(AggregateId id);
+        Task AddAsync(AgendaItem entity);
+    }
+
+    public interface IAgendaTrackRepository
+    {
+
+    }
+}
---
+++ src/Shared/Confab.Shared.Abstractions/Kernel/Types/SpeakerId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Confab.Shared.Abstractions.Kernel.Types.Base;
+
+namespace Confab.Shared.Abstractions.Kernel.Types
+{
+    public class SpeakerId : TypeId
+    {
+        public SpeakerId(Guid id)
+            : base(id)
+        {
+        }
+
+        public static implicit operator SpeakerId(Guid id)
+        {
+            return new SpeakerId(id);
+        }
+    }
+}
