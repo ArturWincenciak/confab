897da92a7ba7ba6ff56dc260de541bb311129e24
TeoVincent <artur.wincenciak@gmail.com>
2021-09-28 19:06:47 +0200

    Create agenda track command handler

---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Commands/Handlers/CreateAgendaTrackHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Confab.Modules.Agendas.Application.Agendas.Events;
+using Confab.Modules.Agendas.Domain.Agendas.Entities;
+using Confab.Modules.Agendas.Domain.Agendas.Repositories;
+using Confab.Shared.Abstractions.Commands;
+using Confab.Shared.Abstractions.Messaging;
+
+namespace Confab.Modules.Agendas.Application.Agendas.Commands.Handlers
+{
+    internal sealed class CreateAgendaTrackHandler : ICommandHandler<CreateAgendaTrack>
+    {
+        private readonly IAgendaTrackRepository _repository;
+        private readonly IMessageBroker _messageBroker;
+
+        public CreateAgendaTrackHandler(IAgendaTrackRepository repository, IMessageBroker messageBroker)
+        {
+            _repository = repository;
+            _messageBroker = messageBroker;
+        }
+
+        public async Task HandleAsync(CreateAgendaTrack command)
+        {
+            var newAgendaTrack = AgendaTrack.Create(command.ConferenceId, command.Name);
+            await _repository.AddAsync(newAgendaTrack);
+            await _messageBroker.PublishAsync(new AgendaTrackCreated(newAgendaTrack.Id));
+        }
+    }
+}
