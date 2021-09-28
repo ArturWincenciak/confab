4d2a062c59344906cbae1b3670e27fe26314263f
TeoVincent <artur.wincenciak@gmail.com>
2021-09-28 18:51:45 +0200

    Assign regular agenda slot command handler

--- src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Commands/Handlers/AssignPlaceholderAgendaSlotHandler.cs
+++ src/Modules/Agendas/Confab.Modules.Agendas.Application/Agendas/Commands/Handlers/AssignPlaceholderAgendaSlotHandler.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Confab.Modules.Agendas.Application.Agendas.Events;
+using Confab.Modules.Agendas.Application.Agendas.Exceptions;
+using Confab.Modules.Agendas.Domain.Agendas.Repositories;
+using Confab.Modules.Agendas.Domain.Agendas.Services;
 using Confab.Shared.Abstractions.Commands;
+using Confab.Shared.Abstractions.Messaging;
 
 namespace Confab.Modules.Agendas.Application.Agendas.Commands.Handlers
 {
     internal sealed class AssignPlaceholderAgendaSlotHandler : ICommandHandler<AssignPlaceholderAgendaSlot>
     {
-        public Task HandleAsync(AssignPlaceholderAgendaSlot command)
+        private readonly IAgendaTrackRepository _repository;
+        private readonly IMessageBroker _messageBroker;
+
+        public AssignPlaceholderAgendaSlotHandler(IAgendaTrackRepository repository, IMessageBroker messageBroker)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+            _messageBroker = messageBroker;
+        }
+
+        public async Task HandleAsync(AssignPlaceholderAgendaSlot command)
+        {
+            var agendaTrack = await _repository.GetAsync(command.AgendaTrackId);
+            if (agendaTrack is null)
+                throw new AgendaTrackNotFoundException(command.AgendaTrackId);
+
+            agendaTrack.ChangeSlotPlaceholder(command.AgendaSlotId, command.Placeholder);
+
+            await _repository.UpdateAsync(agendaTrack);
+
+            var @event = new PlaceholderAssignedToAgendaSlot(command.AgendaSlotId, command.Placeholder);
+            await _messageBroker.PublishAsync(@event);
         }
     }
 }
