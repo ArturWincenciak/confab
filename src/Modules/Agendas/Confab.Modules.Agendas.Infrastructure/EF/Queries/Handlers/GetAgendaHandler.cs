8de4cee8b538880c9195ad8bdb3fdaa09fd1e63a
TeoVincent <artur.wincenciak@gmail.com>
2021-10-26 11:04:52 +0200

    Get agenda query handler

--- src/Modules/Agendas/Confab.Modules.Agendas.Infrastructure/EF/Queries/Handlers/GetAgendaHandler.cs
+++ src/Modules/Agendas/Confab.Modules.Agendas.Infrastructure/EF/Queries/Handlers/GetAgendaHandler.cs
@@ -1,14 +1,61 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Confab.Modules.Agendas.Application.Agendas.Queries;
+using Confab.Modules.Agendas.Domain.Agendas.Entities;
 using Confab.Shared.Abstractions.Queries;
+using Confab.Shared.Abstractions.Storage;
+using Microsoft.EntityFrameworkCore;
 
 namespace Confab.Modules.Agendas.Infrastructure.EF.Queries.Handlers
 {
     internal sealed class GetAgendaHandler : IQueryHandler<GetAgenda, GetAgenda.AgendaDto>
     {
-        public Task<GetAgenda.AgendaDto> HandleAsync(GetAgenda query)
+        private readonly DbSet<AgendaTrack> _agendaTracks;
+        private readonly IRequestStorage _requestStorage;
+
+        public GetAgendaHandler(AgendasDbContext context, IRequestStorage requestStorage)
         {
-            throw new System.NotImplementedException();
+            _agendaTracks = context.AgendaTracks;
+            _requestStorage = requestStorage;
+        }
+
+        public async Task<GetAgenda.AgendaDto> HandleAsync(GetAgenda query)
+        {
+            var storageKey = GetStorageKey(query.ConferenceId);
+            var cached = _requestStorage.Get<GetAgenda.AgendaDto>(storageKey);
+            if (cached is not null)
+                return cached;
+
+            var agendaTracks = await _agendaTracks
+                .Include(x => x.Slots)
+                .ThenInclude(x => (x as RegularAgendaSlot).AgendaItem)
+                .ThenInclude(x => x.Speakers)
+                .Where(x => x.ConferenceId == query.ConferenceId)
+                .ToListAsync();
+
+            var agendaTracksDto = agendaTracks?.Select(x => AsDto(x));
+            var resultDto = new GetAgenda.AgendaDto(agendaTracksDto);
+
+            _requestStorage.Set(storageKey, resultDto, TimeSpan.FromSeconds(5));
+
+            return resultDto;
+        }
+
+        private static GetAgenda.AgendaTrackDto AsDto(AgendaTrack agendaTrack)
+        {
+            return new GetAgenda.AgendaTrackDto
+            (
+                agendaTrack.Id,
+                agendaTrack.ConferenceId,
+                agendaTrack.Name,
+                agendaTrack.Slots
+            );
+        }
+
+        private static string GetStorageKey(Guid conferenceId)
+        {
+            return $"agenda/{conferenceId}";
         }
     }
 }
