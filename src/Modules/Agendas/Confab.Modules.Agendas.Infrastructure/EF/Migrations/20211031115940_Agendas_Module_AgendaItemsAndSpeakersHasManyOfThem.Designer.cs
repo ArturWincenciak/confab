07ac45e5744e07ee2222a77f748c60e5b3ba3764
TeoVincent <artur.wincenciak@gmail.com>
2021-10-31 14:46:01 +0100

    Agenda Items and Speakers has many to many (EF)

--- src/Modules/Agendas/Confab.Modules.Agendas.Infrastructure/EF/Migrations/20211031115940_Agendas_Module_AgendaItemsAndSpeakersHasManyOfThem.Designer.cs
+++ src/Modules/Agendas/Confab.Modules.Agendas.Infrastructure/EF/Migrations/20211031115940_Agendas_Module_AgendaItemsAndSpeakersHasManyOfThem.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Confab.Modules.Agendas.Infrastructure.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Confab.Modules.Agendas.Infrastructure.EF.Migrations
 {
     [DbContext(typeof(AgendasDbContext))]
-    partial class AgendasDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211031115940_Agendas_Module_AgendaItemsAndSpeakersHasManyOfThem")]
+    partial class Agendas_Module_AgendaItemsAndSpeakersHasManyOfThem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Agendas/Confab.Modules.Agendas.Infrastructure/EF/Migrations/20211031115940_Agendas_Module_AgendaItemsAndSpeakersHasManyOfThem.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Confab.Modules.Agendas.Infrastructure.EF.Migrations
+{
+    public partial class Agendas_Module_AgendaItemsAndSpeakersHasManyOfThem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Speakers_AgendaItems_AgendaItemId",
+                schema: "agendas",
+                table: "Speakers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Speakers_AgendaItemId",
+                schema: "agendas",
+                table: "Speakers");
+
+            migrationBuilder.DropColumn(
+                name: "AgendaItemId",
+                schema: "agendas",
+                table: "Speakers");
+
+            migrationBuilder.CreateTable(
+                name: "AgendaItemSpeaker",
+                schema: "agendas",
+                columns: table => new
+                {
+                    AgendaItemsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SpeakersId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AgendaItemSpeaker", x => new { x.AgendaItemsId, x.SpeakersId });
+                    table.ForeignKey(
+                        name: "FK_AgendaItemSpeaker_AgendaItems_AgendaItemsId",
+                        column: x => x.AgendaItemsId,
+                        principalSchema: "agendas",
+                        principalTable: "AgendaItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AgendaItemSpeaker_Speakers_SpeakersId",
+                        column: x => x.SpeakersId,
+                        principalSchema: "agendas",
+                        principalTable: "Speakers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgendaItemSpeaker_SpeakersId",
+                schema: "agendas",
+                table: "AgendaItemSpeaker",
+                column: "SpeakersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AgendaItemSpeaker",
+                schema: "agendas");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AgendaItemId",
+                schema: "agendas",
+                table: "Speakers",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Speakers_AgendaItemId",
+                schema: "agendas",
+                table: "Speakers",
+                column: "AgendaItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Speakers_AgendaItems_AgendaItemId",
+                schema: "agendas",
+                table: "Speakers",
+                column: "AgendaItemId",
+                principalSchema: "agendas",
+                principalTable: "AgendaItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
