1e4b6ed553393dc0292c5deb105f77a3bb822afc
TeoVincent <artur.wincenciak@gmail.com>
2021-12-26 22:23:20 +0100

    Attendances domain ID types

---
+++ src/Modules/Attendances/Confab.Modules.Attendances.Domain/Types/AttendableEventId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Confab.Shared.Abstractions.Kernel.Types.Base;
+
+namespace Confab.Modules.Attendances.Domain.Types
+{
+    public class AttendableEventId : TypeId
+    {
+        public AttendableEventId(Guid id)
+            : base(id)
+        {
+        }
+
+        public static implicit operator AttendableEventId(Guid id)
+            => new(id);
+    }
+}
---
+++ src/Modules/Attendances/Confab.Modules.Attendances.Domain/Types/ParticipantId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Confab.Shared.Abstractions.Kernel.Types.Base;
+
+namespace Confab.Modules.Attendances.Domain.Types
+{
+    public class ParticipantId : TypeId
+    {
+        public ParticipantId(Guid id)
+            : base(id)
+        {
+        }
+
+        public static implicit operator ParticipantId(Guid id)
+            => new(id);
+    }
+}
