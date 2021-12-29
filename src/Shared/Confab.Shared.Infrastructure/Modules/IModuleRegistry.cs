143972d7f39bf380da4b36c7d925e9dfc93fa3d0
TeoVincent <artur.wincenciak@gmail.com>
2021-12-29 12:02:14 +0100

    Module request registration

--- src/Shared/Confab.Shared.Infrastructure/Modules/IModuleRegistry.cs
+++ src/Shared/Confab.Shared.Infrastructure/Modules/IModuleRegistry.cs
@@ -7,6 +7,9 @@ namespace Confab.Shared.Infrastructure.Modules
     public interface IModuleRegistry
     {
         IEnumerable<ModuleBroadcastRegistration> GetBroadcastRegistrations(string key);
+        ModuleRequestRegistration GetRequestRegistration(string path);
+
         void AddBroadcastAction(Type requestType, Func<object, Task> action);
+        void AddRequestAction(string path, Type requestType, Type responseType, Func<object, Task<object>> action);
     }
 }
--- src/Shared/Confab.Shared.Infrastructure/Modules/ModuleRegistry.cs
+++ src/Shared/Confab.Shared.Infrastructure/Modules/ModuleRegistry.cs
@@ -8,10 +8,16 @@ namespace Confab.Shared.Infrastructure.Modules
     public class ModuleRegistry : IModuleRegistry
     {
         private readonly List<ModuleBroadcastRegistration> _broadcastRegistrations = new();
+        private readonly Dictionary<string, ModuleRequestRegistration> _requestRegistrations = new();
 
         public IEnumerable<ModuleBroadcastRegistration> GetBroadcastRegistrations(string key)
         {
             return _broadcastRegistrations.Where(x => x.Key == key);
+        }
+
+        public ModuleRequestRegistration GetRequestRegistration(string path)
+        {
+            return _requestRegistrations.TryGetValue(path, out var registration) ? registration : null;
         }
 
         public void AddBroadcastAction(Type requestType, Func<object, Task> action)
@@ -21,6 +27,15 @@ namespace Confab.Shared.Infrastructure.Modules
 
             var registration = new ModuleBroadcastRegistration(requestType, action);
             _broadcastRegistrations.Add(registration);
+        }
+
+        public void AddRequestAction(string path, Type requestType, Type responseType, Func<object, Task<object>> action)
+        {
+            if (path is null)
+                throw new InvalidOperationException("Request path cannot be null.");
+
+            var registration = new ModuleRequestRegistration(requestType, responseType, action);
+            _requestRegistrations.Add(path, registration);
         }
     }
 }
