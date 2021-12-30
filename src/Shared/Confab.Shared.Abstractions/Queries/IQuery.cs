b4509889b0d6c937f6677bdc139ec4a0eddb9665
TeoVincent <artur.wincenciak@gmail.com>
2021-12-30 18:42:50 +0100

    Move IModuleRequest/Response to its own files

--- src/Shared/Confab.Shared.Abstractions/Queries/IQuery.cs
+++ src/Shared/Confab.Shared.Abstractions/Queries/IQuery.cs
@@ -7,13 +7,4 @@
     public interface IQuery<TResult> : IQuery where TResult : class, IQueryResult
     {
     }
-
-
-    public interface IModuleResponse
-    {
-    }
-
-    public interface IModuleRequest
-    {
-    }
 }
