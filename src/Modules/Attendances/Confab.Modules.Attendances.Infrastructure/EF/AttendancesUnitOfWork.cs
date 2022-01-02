16929863393af3ef61b518c6965cef583f3d2f2c
TeoVincent <artur.wincenciak@gmail.com>
2022-01-02 16:05:19 +0100

    Log rollback db transaction

--- src/Modules/Attendances/Confab.Modules.Attendances.Infrastructure/EF/AttendancesUnitOfWork.cs
+++ src/Modules/Attendances/Confab.Modules.Attendances.Infrastructure/EF/AttendancesUnitOfWork.cs
@@ -1,11 +1,12 @@
 ﻿using Confab.Shared.Infrastructure.Postgres;
+using Microsoft.Extensions.Logging;
 
 namespace Confab.Modules.Attendances.Infrastructure.EF
 {
     internal class AttendancesUnitOfWork : PostgresUnitOfWork<AttendancesDbContext>, IAttendancesUnitOfWork
     {
-        public AttendancesUnitOfWork(AttendancesDbContext dbContext)
-            : base(dbContext)
+        public AttendancesUnitOfWork(AttendancesDbContext dbContext, ILogger<AttendancesUnitOfWork> logger)
+            : base(dbContext, logger)
         {
         }
     }
--- src/Shared/Confab.Shared.Infrastructure/Postgres/PostgresUnitOfWork.cs
+++ src/Shared/Confab.Shared.Infrastructure/Postgres/PostgresUnitOfWork.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
 
 namespace Confab.Shared.Infrastructure.Postgres
 {
     public abstract class PostgresUnitOfWork<T> : IUnitOfWork where T : DbContext
     {
         private readonly T _dbContext;
+        private readonly ILogger<PostgresUnitOfWork<T>> _logger;
 
-        protected PostgresUnitOfWork(T dbContext)
+        protected PostgresUnitOfWork(T dbContext, ILogger<PostgresUnitOfWork<T>> logger)
         {
             _dbContext = dbContext;
+            _logger = logger;
         }
 
         public async Task ExecuteAsync(Func<Task> action)
@@ -21,9 +25,23 @@ namespace Confab.Shared.Infrastructure.Postgres
                 await action();
                 await transaction.CommitAsync();
             }
-            catch (Exception)
+            catch (Exception ex)
+            {
+                await RollbackWithLogsAsync(transaction, ex);
+                throw;
+            }
+        }
+
+        private async Task RollbackWithLogsAsync(IDbContextTransaction transaction, Exception exContext)
+        {
+            try
             {
                 await transaction.RollbackAsync();
+                _logger.LogWarning(exContext, $"Rollback DB transaction {transaction.TransactionId} applied.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Rollback DB transaction {transaction.TransactionId} failed.");
                 throw;
             }
         }
