22298c5d232d1ec9b4f73c477bb2146321cac366
TeoVincent <artur.wincenciak@gmail.com>
2022-02-05 12:32:15 +0100

    First empty example event

---
+++ tests/Confab.Modules.Attendances.Tests.Unit/Entities/AttendableEventAttendTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace Confab.Modules.Attendances.Tests.Unit.Entities
+{
+    public class AttendableEventAttendTests
+    {
+        [Fact]
+        public void t1()
+        {
+            // arrange
+
+            // act
+
+            // assert
+
+        }
+
+
+    }
+}
