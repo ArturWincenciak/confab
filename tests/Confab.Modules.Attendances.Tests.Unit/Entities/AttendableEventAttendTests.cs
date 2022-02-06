a045211103de8d441428833ee224ec27ce8cbfce
TeoVincent <artur.wincenciak@gmail.com>
2022-02-06 11:37:54 +0100

    Remove empty test method template
    This is commit made with Fork tool

--- tests/Confab.Modules.Attendances.Tests.Unit/Entities/AttendableEventAttendTests.cs
+++ tests/Confab.Modules.Attendances.Tests.Unit/Entities/AttendableEventAttendTests.cs
@@ -4,14 +4,7 @@ namespace Confab.Modules.Attendances.Tests.Unit.Entities
 {
     public class AttendableEventAttendTests
     {
-        [Fact]
-        public void t1()
-        {
-            // arrange
 
-            // act
-
-            // assert
 
         }
 
