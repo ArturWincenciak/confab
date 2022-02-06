075e220c4accb3fa83097f4f154f9ac8f5f61b6d
TeoVincent <artur.wincenciak@gmail.com>
2022-02-06 16:19:04 +0100

    Refact unit tests: extract arrange and with... method

--- tests/Confab.Modules.Attendances.Tests.Unit/Entities/AttendableEventAttendTests.cs
+++ tests/Confab.Modules.Attendances.Tests.Unit/Entities/AttendableEventAttendTests.cs
@@ -8,10 +8,11 @@ namespace Confab.Modules.Attendances.Tests.Unit.Entities
 {
     public class AttendableEventAttendTests
     {
-        private readonly AttendableEvent _target;
-        private readonly Participant _participant;
+        private AttendableEvent _target;
+        private Participant _participant_1;
+        private Slot _slot_1;
 
-        public AttendableEventAttendTests()
+        private void Arrange()
         {
             var conferenceId = Guid.Parse("44C6F685-1E79-490A-B0B1-F3BE3545C990");
             var attendableEventId = Guid.Parse("962C87AD-9FC0-45C4-B38D-98F215271746");
@@ -19,21 +20,32 @@ namespace Confab.Modules.Attendances.Tests.Unit.Entities
             var to = new DateTime(2022, 2, 6, 12, 0, 0);
             _target = AttendableEvent.Create(attendableEventId, conferenceId, from, to);
             var userId = Guid.Parse("BDF65461-9944-4B31-9EC3-C43EA63CFB7F");
-            _participant = Participant.Create(conferenceId, userId, attendances: null);
+            _participant_1 = Participant.Create(conferenceId, userId, attendances: null);
+            _slot_1 = new Slot(Guid.Parse("39720949-CD62-4EFA-B1C8-ABBDF8F7334B"));
+        }
+
+        private void WithSlot()
+        {
+            _target.AddSlots(new []{_slot_1});
+        }
+
+        private void WithTakeSlot()
+        {
+            _slot_1.Take(_participant_1.Id);
         }
 
         private Attendance Act()
         {
-            return _target.Attend(_participant);
+            return _target.Attend(_participant_1);
         }
 
         [Fact]
         public void Given_No_Slots_When_Attend_Then_Should_Fail()
         {
-            // act
+            Arrange();
+
             var actual = Record.Exception(Act);
 
-            // assert
             actual.ShouldNotBeNull();
             actual.ShouldBeOfType<NoFreeSlotsException>();
             _target.Slots.ShouldBeEmpty();
@@ -42,17 +54,17 @@ namespace Confab.Modules.Attendances.Tests.Unit.Entities
         [Fact]
         public void Given_Slots_With_Participant_When_Attend_Then_Should_Fail()
         {
-            // arrange
-            var slotId = Guid.Parse("B807CF93-9BD2-49A5-999E-F2635A5E67FE");
-            _target.AddSlots(new []{ new Slot(slotId, _participant.Id)});
+            Arrange();
+            WithSlot();
+            WithTakeSlot();
 
-            // act
             var actual = Record.Exception(Act);
 
-            // assert
             actual.ShouldNotBeNull();
             actual.ShouldBeOfType<AlreadyParticipatingInEventException>();
             _target.Slots.ShouldNotBeEmpty();
         }
+
+
     }
 }
