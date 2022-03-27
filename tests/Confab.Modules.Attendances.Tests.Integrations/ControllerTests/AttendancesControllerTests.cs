a4a44eb794bf72a9e2134a6acb4eb8137101f266
TeoVincent <artur.wincenciak@gmail.com>
2022-03-27 23:01:20 +0200

    TODO

--- tests/Confab.Modules.Attendances.Tests.Integrations/ControllerTests/AttendancesControllerTests.cs
+++ tests/Confab.Modules.Attendances.Tests.Integrations/ControllerTests/AttendancesControllerTests.cs
@@ -34,5 +34,59 @@ namespace Confab.Modules.Attendances.Tests.Integrations.ControllerTests
             // assert
             actual.ShouldBeNotFound();
         }
+
+        //TODO
+        //[Fact]
+        //public async Task get_browse_attendances_given_valid_conference_and_participant_should_return_all_attendances()
+        //{
+        //    var from = DateTime.UtcNow;
+        //    var to = from.AddDays(1);
+        //    var conferenceId = Guid.NewGuid();
+        //    var userId = Guid.NewGuid();
+        //    var participant = new Participant(Guid.NewGuid(), conferenceId, userId);
+        //    var slot = new Slot(Guid.NewGuid(), participant.Id);
+        //    var attendableEvent = new AttendableEvent(Guid.NewGuid(), conferenceId, from, to, new[] {slot});
+        //    var attendance = new Attendance(Guid.NewGuid(), attendableEvent.Id, slot.Id, participant.Id, from, to);
+
+        //    await _dbContext.AttendableEvents.AddAsync(attendableEvent);
+        //    await _dbContext.Attendances.AddAsync(attendance);
+        //    await _dbContext.Participants.AddAsync(participant);
+        //    await _dbContext.Slots.AddAsync(slot);
+        //    await _dbContext.SaveChangesAsync();
+
+        //    _agendasApiClient.GetAgendaAsync(conferenceId).Returns(new List<AgendaTrackDto>
+        //    {
+        //        new()
+        //        {
+        //            Id = Guid.NewGuid(),
+        //            Name = "Track 1",
+        //            ConferenceId = conferenceId,
+        //            Slots = new[]
+        //            {
+        //                new RegularAgendaSlotDto
+        //                {
+        //                    Id = Guid.NewGuid(),
+        //                    From = from,
+        //                    To = to,
+        //                    AgendaItem = new AgendaItemDto
+        //                    {
+        //                        Id = attendableEvent.Id,
+        //                        Title = "test",
+        //                        Description = "test",
+        //                        Level = 1
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    });
+
+        //    Authenticate(userId);
+        //    var response = await _client.GetAsync($"{Path}/{conferenceId}");
+        //    response.IsSuccessStatusCode.ShouldBeTrue();
+
+        //    var attendances = await response.Content.ReadFromJsonAsync<AttendanceDto[]>();
+        //    attendances.ShouldNotBeEmpty();
+        //    attendances.Length.ShouldBe(1);
+        //}
     }
 }
