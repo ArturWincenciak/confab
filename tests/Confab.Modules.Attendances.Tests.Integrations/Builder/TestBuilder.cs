c9e7948a5bc734fdc72a42ead4f4c7c6f9f0fdcb
TeoVincent <artur.wincenciak@gmail.com>
2022-04-03 22:11:23 +0200

    Ensure database deleted on start new Unit Test

--- tests/Confab.Modules.Attendances.Tests.Integrations/Builder/TestBuilder.cs
+++ tests/Confab.Modules.Attendances.Tests.Integrations/Builder/TestBuilder.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Confab.Modules.Attendances.Tests.Integrations.Builder.Api;
 using Confab.Shared.Tests;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Confab.Modules.Attendances.Tests.Integrations.Builder
@@ -12,38 +14,46 @@ namespace Confab.Modules.Attendances.Tests.Integrations.Builder
     {
         private readonly List<Action> _actions = new();
         private HttpClient _client;
-        private TestApplicationFactory _applicationFactory;
 
         public TestApplication Build()
         {
-            _applicationFactory = new TestApplicationFactory();
-            _applicationFactory.WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureServices(services =>
+            _client = new TestApplicationFactory()
+                .WithWebHostBuilder(builder =>
                 {
-                    // do something here if needed ...
-                    //var serviceProvider = services.BuildServiceProvider();
-                    //using var scope = serviceProvider.CreateScope();
-                    //var scopedServices = scope.ServiceProvider;
-                    //var db = scopedServices.GetRequiredService<UsersDbContext>();
+                    builder.ConfigureServices(services =>
+                    {
+                        EnsureDatabaseDeleted(services);
+                    });
+                })
+                .CreateClient();
 
-                    //try
-                    //{
-                    //    Utilities.ReinitializeDbForTests(db);
-                    //}
-                    //catch (Exception ex)
-                    //{
-
-                    //}
-                });
-            });
-
-            _client = _applicationFactory.CreateClient();
 
             foreach (var action in _actions)
                 action();
 
             return new TestApplication(_client);
+        }
+
+        private static void EnsureDatabaseDeleted(IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            using var scope = serviceProvider.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            var dbContextType = GetSomeFirstDbContextType();
+            var db = scopedServices.GetRequiredService(dbContextType) as DbContext;
+            db.Database.EnsureDeleted();
+        }
+
+        private static Type GetSomeFirstDbContextType()
+        {
+            var dbContextType = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .First(x =>
+                    typeof(DbContext).IsAssignableFrom(x) &&
+                    !x.IsInterface &&
+                    x != typeof(DbContext));
+            return dbContextType;
         }
 
         public TestBuilder WithAuthentication()
