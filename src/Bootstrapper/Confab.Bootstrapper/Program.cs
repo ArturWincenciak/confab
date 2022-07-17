8ad775b5a8e746d5fe57603567edd7409af6e984
TeoVincent <artur.wincenciak@gmail.com>
2022-07-18 00:26:57 +0200

    Cleanup namespace

--- src/Bootstrapper/Confab.Bootstrapper/Program.cs
+++ src/Bootstrapper/Confab.Bootstrapper/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Confab.Shared.Infrastructure.Modules;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
