18a6ad21154cbd8af50b1b25d82570f84c2c4d9d
TeoVincent <artur.wincenciak@gmail.com>
2022-07-18 00:40:28 +0200

    Create new Saga project

---
+++ src/Modules/Saga/Confab.Modules.Saga/SagaModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Confab.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Modules.Saga
+{
+    public class SagaModule : IModule
+    {
+        public const string BasePath = "saga-module";
+
+        public string Name { get; } = "Saga";
+
+        public string Path => BasePath;
+
+        public IEnumerable<string> Policies { get; }
+
+        public void Register(IServiceCollection services)
+        {
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+        }
+    }
+}
