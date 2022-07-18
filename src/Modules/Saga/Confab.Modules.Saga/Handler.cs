c4266302e0dde0d3dc08fb102a762d9f12cf5264
TeoVincent <artur.wincenciak@gmail.com>
2022-07-18 23:52:52 +0200

    Saga messages

---
+++ src/Modules/Saga/Confab.Modules.Saga/Handler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Confab.Modules.Saga.Messages;
+using Confab.Shared.Abstractions.Events;
+
+namespace Confab.Modules.Saga
+{
+    internal class Handler : IEventHandler<SpeakerCreated>, IEventHandler<SignedIn>, IEventHandler<SignedUp>
+    {
+        public Task HandleAsync(SpeakerCreated @event)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task HandleAsync(SignedIn @event)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task HandleAsync(SignedUp @event)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Modules/Saga/Confab.Modules.Saga/Messages/SpeakerCreated.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using Confab.Shared.Abstractions.Events;
+
+namespace Confab.Modules.Saga.Messages
+{
+    internal record SpeakerCreated(Guid Id, string FullName) : IEvent;
+}
